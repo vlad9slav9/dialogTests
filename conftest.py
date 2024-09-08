c2be14d31b216a6e6671a6a046a55b162cab5500
vlad vusaty <vladel9ts@gmail.com>
2024-09-08 12:42:27 +0300

    add project on github

---
+++ conftest.py
@@ -0,0 +1,24 @@
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', help="Choose browser: gost or yandex")
+
+@pytest.fixture(scope="session")
+def browser(playwright, request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "gost":
+        browser = playwright.chromium.launch(executable_path=r"C:\Program Files (x86)\krtech\chrome\chrome.exe", headless=False)
+    elif browser_name == "yandex":
+        browser = playwright.chromium.launch(executable_path=r"C:\Users\KSK-SHOP\AppData\Local\Yandex\YandexBrowser\Application\browser.exe", headless=False)
+    else:
+        browser = playwright.chromium.launch()
+    yield browser
+    browser.close()
+
+@pytest.fixture(scope="function")
+def login_page(page):
+    login_page = LoginPage(page)
+    login_page.navigate()
+    yield login_page
