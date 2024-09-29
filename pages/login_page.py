2815967afbf92ad38aa9cd4431f680be100f0796
vlad vusaty <vladel9ts@gmail.com>
2024-09-29 14:19:13 +0300

    add incorrect username and password tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 import configparser
 
-from playwright.async_api import Page
+from playwright.sync_api import Page
 from playwright.sync_api import expect
 
 from pages.event_page import EventPage
@@ -32,4 +32,18 @@ class LoginPage:
         self._username_input.fill(username)
         self._password_input.fill(password)
         self._login_button.click()
+
+    def get_responsible_username(self):
+        return self.config['responsible']['username']
+
+    def get_responsible_password(self):
+        return self.config['responsible']['password']
+
+    def login_with_responsible(self):
+        username = self.get_responsible_username()
+        password = self.get_responsible_password()
+        self.do_login(username, password)
         return EventPage(self.page)
+
+    def assert_login_error_visible(self):
+        expect(self._login_error_message).to_be_visible()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,23 @@
+from faker import Faker
+
+fake = Faker()
+fake_ru = Faker("ru_RU")
+
 
 def test_correct_login(login_page):
-    event_page = login_page.do_login("devyt9", "12332145")
-    event_page.assert_user_profile_button_visible()
+    event_page = login_page.login_with_responsible()
+    event_page.assert_responsible_profile_button_visible()
+
+
+def test_incorrect_login(login_page):
+    fake_username = fake.user_name()
+    correct_password = login_page.get_responsible_password()
+    login_page.do_login(fake_username, correct_password)
+    login_page.assert_login_error_visible()
+
+
+def test_incorrect_password(login_page):
+    correct_username = login_page.get_responsible_username()
+    fake_password = fake.password()
+    login_page.do_login(correct_username, fake_password)
+    login_page.assert_login_error_visible()
